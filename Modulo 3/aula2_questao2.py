0eab163e2ebbdb4e17218197f285cad198505c37
Juninho <juliocouto.engprod@gmaol.com>
2024-05-26 16:19:15 -0300

    ok

---
+++ Modulo 3/aula2_questao2.py
@@ -0,0 +1,8 @@
+#solicitando a idade de ambas 
+idade1 = int (input("Digite a idade da Juliana: "))
+idade2 = int (input("Digite a idade da Cris: "))
+
+#verificando a permisão
+permisao= (idade1>17) or  (idade2>17)
+print (permisao)
+
