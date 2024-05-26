a7866d4a936cb861a48044e2e1f7befbbb72a37a
Juninho <juliocouto.engprod@gmaol.com>
2024-05-26 12:22:02 -0300

    questao2

---
+++ Modulo 2/aula2_questao.py
@@ -0,0 +1,16 @@
+resultado = "o resultado é "
+n1=10
+n2=3.5
+soma=-(n1+n2)
+
+#/testet/#
+
+print (resultado)
+print (type(resultado))
+print (n1)
+print (type(n1))
+print (n2)
+print (type(n2))
+print (soma)
+print (type(soma))
+
