e2df9fe8ab5683a32bce17591c1a209d18778350
Juninho <juliocouto.engprod@gmaol.com>
2024-05-26 14:38:36 -0300

    teste89

--- Modulo 2/aula4_questao1.py
+++ Modulo 2/aula4_questao1.py
@@ -1,8 +1,12 @@
+
+# solicitando o usuario a entrada de dados, especificando qual o tipo, para garantir que o calculo ocorra
 comprimento= int (input ('Digite o comprimento do terreno:'))
 largura= int(input ('Digite a largura do terreno:'))
 preco= float (input ('Digite o preço por metro quadrado:'))
 
+# realizado os calculos necessarios de acordo com as entrandas fornecidas
 area = comprimento*largura
 preco_total = preco*area
 
+# apresentando ao usuario a solucação. 
 print (f"O terreno possui {area} m2 e custa R$' {preco_total}.")
