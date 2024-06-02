6441aa672c80aa77d986c0b1b167d7989c65d941
Juninho <juliocouto.engprod@gmaol.com>
2024-06-02 14:57:37 -0300

    questa23

--- Modulo 3/aula2_questao3.py
+++ Modulo 3/aula2_questao3.py
@@ -3,5 +3,4 @@ jogo = int (input("Já jogou pelo menos 3 jogos de tabuleiro? "))
 vitoria = int (input("Quantos jogos já venceu?"))
 
 permisao= ((idade>=16 and idade<=18) and (jogo >=3) and (vitoria >=1))
-
 print (f"Apto para ingressar no clube de jogos de tabuleiro: {permisao}")
